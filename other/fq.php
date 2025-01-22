d94727cdc0a2f25f332948f5736e8817bcd9d3bd
Christian Milanes <christianmilanes01@gmail.com>
2025-01-22 02:38:57 +0100

    fq page created

---
+++ other/fq.php
@@ -0,0 +1,43 @@
+<?php
+include('../includes/head.php');
+?>
+<title>INETE | Preguntas frecuentes</title>
+
+<!--::::::::::::::: Meta tags here :::::::::::::::-->
+<meta name="description" content="Preguntas frecuentes sobre nacionalidad española para extranjeros">
+<meta name="robots" content="index, follow">
+<meta name="googlebot" content="index, follow">
+<link rel="canonical" href="https://inete.com/other/fq.php">
+
+<!-- Open Graph para Facebook y otros -->
+<meta property="og:title" content="INETE | Preguntas frecuentes">
+<meta property="og:description" content="Preguntas frecuentes sobre nacionalidad española para extranjeros">
+<!-- <meta property="og:image" content="https://www.tusitio.com/images/homepage.jpg"> -->
+<meta property="og:url" content="https://inete.com/other/fq.php">
+<meta property="og:type" content="website">
+
+<!-- Twitter Cards -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title" content="INETE | Preguntas frecuentes">
+<meta name="twitter:description" content="Preguntas frecuentes sobre nacionalidad española para extranjeros">
+<!-- <meta name="twitter:image" content="https://www.tusitio.com/images/homepage.jpg"> -->
+
+<?php
+include('../includes/head-end.php');
+?>
+
+<?php
+include('../includes/header.php');
+?>
+
+<!--::::::::::::::: Body page here :::::::::::::::-->
+<div class="container-fluid p-0 m-0 mt-3">
+  <?php
+  include('../includes/pages-body/other/fq.inc.php');
+  ?>
+</div>
+
+
+<?php
+include('../includes/foot.php');
+?>
