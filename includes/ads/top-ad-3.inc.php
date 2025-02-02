adca6d20e5a2f9f0ad8336b5338e45011a6a48a5
Christian Milanes <christianmilanes01@gmail.com>
2025-02-02 14:56:27 +0100

    updated

--- includes/ads/top-ad-3.inc.php
+++ includes/ads/top-ad-3.inc.php
@@ -1,8 +1,9 @@
 <div
-  class="row justify-content-center align-items-center m-0 p-0 to-movil-ad text-black fw-bold border rounded bg-light bg-opacity-75 text-center">
+  class="row justify-content-center align-items-center m-0 p-0 no-movil-ad text-black fw-bold border rounded bg-light bg-opacity-75">
   <div class="col-12">
     <div class="mt-2 mb-2">
       <h2 class="text-center">ANUNCIOS</h2>
+      <p class="text-danger fw-bold">Desplazar hacia abajo para ver contenido</p>
     </div>
   </div>
   <div class="col-12 m-0 p-0">
