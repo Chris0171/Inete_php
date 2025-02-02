bfe0ce7c45ab30f8cc1532225fae8f8176d64342
Christian Milanes <christianmilanes01@gmail.com>
2025-02-02 13:55:56 +0100

    new ads include created

---
+++ includes/ads/top-ad-3.inc.php
@@ -0,0 +1,5 @@
+<div class="to-movil-ad text-black fw-bold border rounded bg-light bg-opacity-75">
+  <script async="async" data-cfasync="false"
+    src="//pl25742019.profitablecpmrate.com/fc1a754eb38ec053b2fbddb3de51c40a/invoke.js"></script>
+  <div id="container-fc1a754eb38ec053b2fbddb3de51c40a"></div>
+</div>
