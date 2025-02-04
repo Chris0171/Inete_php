0d7e4e5c1c06754b1712842f927f6c65627a9504
Christian Milanes <christianmilanes01@gmail.com>
2025-02-04 12:41:35 +0100

    new add container added

---
+++ includes/ads/section-1.inc.php
@@ -0,0 +1,16 @@
+<div>
+  <h3 class="fw-bold text-center mb-2">Anuncios</h3>
+</div>
+<div class="ad-section-container">
+  <script type="text/javascript">
+  atOptions = {
+    'key': '6ca5b3e61962834e1fc197892ec58b0b',
+    'format': 'iframe',
+    'height': 50,
+    'width': 320,
+    'params': {}
+  };
+  </script>
+  <script type="text/javascript" src="//www.highperformanceformat.com/6ca5b3e61962834e1fc197892ec58b0b/invoke.js">
+  </script>
+</div>
