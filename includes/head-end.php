15ac0462c9039a4a81fa259b8c115dd7bbb9c642
Christian Milanes <christianmilanes01@gmail.com>
2025-02-06 18:26:52 +0100

    popunder added again

--- includes/head-end.php
+++ includes/head-end.php
@@ -1,6 +1,6 @@
-<!-- Popunder ads
+<!-- Popunder ads -->
 <script type='text/javascript' src='//pl25753271.profitablecpmrate.com/66/60/01/666001f1e039b4b641f4554032a9a218.js'>
-</script> -->
+</script>
 
 </head>
 
